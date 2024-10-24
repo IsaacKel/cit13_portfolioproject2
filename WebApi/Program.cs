1b64fca28b4ed72103ef467aad5f65a268fc6bf7
IsaacKel <s3931572@student.rmit.edu.au>
2024-10-24 11:29:03 +0200

    more base files

---
+++ WebApi/Program.cs
@@ -0,0 +1,40 @@
+using DataLayer;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Register MovieDatabase with the dependency injection container
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MovieDatabase")));
+
+// Register IDataService and the DataService implementation
+builder.Services.AddSingleton<IDataService, DataService>();
+
+// Mapster configuration
+builder.Services.AddMapster();
+
+builder.Services.AddControllers();
+
+// Swagger configuration for API documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
