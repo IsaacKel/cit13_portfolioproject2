87de189348fe87e398fcafa99d796c1fade11506
IsaacKel <s3931572@student.rmit.edu.au>
2024-10-31 18:01:08 +0100

    small changes

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6b448747be9a09843fcd8583ac8c981ee91109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabd5e7dc57fd540e122e18f960fa5f7972f9224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
