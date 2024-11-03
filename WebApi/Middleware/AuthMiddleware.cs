61a96e494a6495e2d6c8a75abe792cab5743915c
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2024-11-03 19:08:01 +0100

    Added Middleware folder and AuthMiddleware script

---
+++ WebApi/Middleware/AuthMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Middleware
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+        
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+             await _next(context);
+        }
+    }
+}
