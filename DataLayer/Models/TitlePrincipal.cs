87979201623d7d574a70e73269c02ac366348f57
IsaacKel <s3931572@student.rmit.edu.au>
2024-11-05 11:48:42 +0100

    fix job is null bug

--- DataLayer/Models/TitlePrincipal.cs
+++ DataLayer/Models/TitlePrincipal.cs
@@ -12,6 +12,6 @@ namespace DataLayer.Models
         public string NConst { get; set; }
         public int Ordering { get; set; }
         public string Category { get; set; }
-        public string Job { get; set; }
+        public string? Job { get; set; }
     }
 }
