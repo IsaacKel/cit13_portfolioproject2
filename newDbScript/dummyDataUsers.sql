8e51e6359ac4741ac96c4a36454869a24e108707
IsaacKel <s3931572@student.rmit.edu.au>
2024-11-05 16:10:21 +0100

    adjusted script to add Users

--- newDbScript/dummyDataUsers.sql
+++ newDbScript/dummyDataUsers.sql
@@ -1,9 +1,3 @@
--- Insert test data into users table
-INSERT INTO users (userid, username, email, password) OVERRIDING SYSTEM VALUE VALUES
-(1, 'john_doe', 'john.doe@example.com', 'password123'),
-(2, 'jane_smith', 'jane.smith@example.com', 'password456'),
-(3, 'alice_jones', 'alice.jones@example.com', 'password789');
-
 -- Insert test data into userRatings table
 INSERT INTO userRatings (userratingid, userId, tconst, rating, ratingDate) OVERRIDING SYSTEM VALUE VALUES
 (1, 1, 'tt28638980', 8, NOW() AT TIME ZONE 'UTC'),
@@ -11,7 +5,7 @@ INSERT INTO userRatings (userratingid, userId, tconst, rating, ratingDate) OVERR
 (3, 1, 'tt28638980', 10, NOW() AT TIME ZONE 'UTC');
 
 -- Insert test data into userSearchHistory table
-INSERT INTO userSearchHistory (searchid, userId, searchQuery, searchDate) OVERRIDING SYSTEM VALUE VALUES
+INSERT INTO usersearchHistory (searchid, userId, searchQuery, searchDate) OVERRIDING SYSTEM VALUE VALUES
 (1, 1, 'Action movies', NOW() AT TIME ZONE 'UTC'),
 (2, 2, 'Comedy films', NOW() AT TIME ZONE 'UTC'),
 (3, 1, 'Drama series', NOW() AT TIME ZONE 'UTC');
@@ -21,3 +15,9 @@ INSERT INTO userBookmarks (bookmarkid, userId, tconst, nconst, note, bookmarkDat
 (1, 1, 'tt28638980', null, 'Great movie to watch!', NOW() AT TIME ZONE 'UTC'),
 (2, 2, 'tt28638980', null, 'Recommended for friends.', NOW() AT TIME ZONE 'UTC'),
 (3, 3, null, 'nm0000703', 'Loved the storyline!', NOW() AT TIME ZONE 'UTC');
+
+INSERT INTO users (userid, username, email, password, salt, name, role) OVERRIDING SYSTEM VALUE 
+VALUES 
+(1, 'user1', 'user1@example.com', 'hashedpassword1', 'randomsalt1', 'User One', 'Admin'),
+(2, 'user2', 'user2@example.com', 'hashedpassword2', 'randomsalt2', 'User Two', 'User'),
+(3, 'user3', 'user3@example.com', 'hashedpassword3', 'randomsalt3', 'User Three', 'Moderator'); 
