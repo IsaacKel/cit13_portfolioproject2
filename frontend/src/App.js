c380dbbf95a55b22e89fc7c42777ea49c323649d
IsaacKel <s3931572@student.rmit.edu.au>
2024-11-22 12:11:55 +0100

    installed bootstrap and imported

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <a
+      className="btn btn-primary"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  );
+}
+
+export default App;
