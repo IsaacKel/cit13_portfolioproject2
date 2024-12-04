df0acd4244a1526f48e273c4970eeda9b2883356
NIZZSHARMA <nizzsharma10@gmail.com>
2024-12-04 16:39:33 +0100

    Change footer color

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-light text-center py-3">
+    <footer className="bg-dark text-center py-3">
       <p>Copyright &copy; 2024. All Rights Reserved.</p>
     </footer>
   );
