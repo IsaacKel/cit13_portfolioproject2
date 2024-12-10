b35331f646d8f5a10aa04a5d77967fcf46b7e083
mkmason <78545722+mkmason@users.noreply.github.com>
2024-12-11 00:26:40 +0100

    all data showing on user page

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -4,6 +4,9 @@ import {
   fetchBookmarks,
   fetchUserRatings,
   fetchUserSearchHistory,
+  fetchTitleData,
+  fetchNameData,
+  fetchImages,
 } from "../services/apiService";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../components/AuthContext";
@@ -35,25 +38,46 @@ const UserPage = () => {
             fetchUserRatings(),
             fetchUserSearchHistory(),
           ]);
-        setUser(userData);
-        setBookmarks(userBookmarks);
-        setRatings(userRatings);
-        setSearchHistory(userSearchHistory);
+  
+        setUser(userData || {});
+        const bookmarksWithTitles = await Promise.all(
+          userBookmarks.items.map(async (bookmark) => {
+            if (bookmark.tConst) {
+              const titleData = await fetchTitleData(bookmark.tConst);
+              return { ...bookmark, title: titleData };
+            }else if (bookmark.nConst) {
+              const nameData = await fetchNameData(bookmark.nConst);
+              const nameImage = await fetchImages(bookmark.nConst);
+              return { ...bookmark, name: nameData, image: nameImage };
+            }
+            return bookmark;
+          })
+        );
+        console.log(bookmarksWithTitles);
+        setBookmarks({ items: bookmarksWithTitles });
+        const ratingsWithTitles = await Promise.all(
+          userRatings.items.map(async (rating) => {
+            const titleData = await fetchTitleData(rating.tConst);
+            return { ...rating, title: titleData };
+          })
+        );
+        setRatings(ratingsWithTitles || []);
+        setSearchHistory(userSearchHistory || []);
       } catch (err) {
         setError(err.message);
       } finally {
         setLoading(false);
       }
     };
-
+  
     fetchData();
-  }, []); // Remove userID from dependencies since it's no longer needed
+  }, []);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
+    <div style={{color: 'white'}}>
       <h1>User Page</h1>
       {user && (
         <div>
@@ -81,11 +105,35 @@ const UserPage = () => {
                   <Link to={`/title/${bookmark.tConst}`}>
                     {bookmark.tConst}
                   </Link>
+                  <p>
+                    <strong>Title Name:</strong> {bookmark.title.primaryTitle}
+                  </p>
+                  <p>
+                    <strong>Title Type:</strong> {bookmark.title.titleType}
+                  </p>
+                  <p>
+                    <strong>Poster: </strong>{<img src={bookmark.title.poster} alt="poster" style={{ width: '100px', height: 'auto' }} />}
+                  </p>
                 </>
               ) : (
                 <>
                   <strong>Name:</strong>{" "}
                   <Link to={`/name/${bookmark.nConst}`}>{bookmark.nConst}</Link>
+                  <p>
+                    <strong>Name:</strong> {bookmark.name.actualName}
+                  </p>
+                  <p>
+                    <strong>Birth Year:</strong>{bookmark.name.birthYear}
+                  </p>
+                  <p>
+                    <strong>Death Year:</strong>{bookmark.name.deathYear}
+                  </p>
+                  <p>
+                    <string>Rating: </string>{bookmark.name.nRating}
+                  </p>
+                  <p>
+                    <strong>Image: </strong>{<img src={bookmark.image} alt="image" style={{ width: '100px', height: 'auto' }} />}
+                  </p>
                 </>
               )}
               <p>
@@ -106,12 +154,12 @@ const UserPage = () => {
       )}
 
       <h2>Ratings</h2>
-      {ratings.items && ratings.items.length > 0 ? (
+      {ratings && ratings.length > 0 ? (
         <ul>
-          {ratings.items.map((rating) => (
+          {ratings.map((rating) => (
             <li key={rating.id}>
               <p>
-                <strong>Title:</strong>{" "}
+                <strong>Title ID:</strong>{" "}
                 <Link to={`/title/${rating.tConst}`}>{rating.tConst}</Link>
               </p>
               <p>
@@ -123,6 +171,15 @@ const UserPage = () => {
               <p>
                 <strong>Date:</strong>{" "}
                 {new Date(rating.createdAt).toLocaleDateString("en-GB")}
+              </p>
+              <p>
+                <strong>Title Name:</strong> {rating.title.primaryTitle}
+              </p>
+              <p>
+                <strong>Title Type:</strong> {rating.title.titleType}
+              </p>
+              <p>
+                <strong>Poster: </strong>{<img src={rating.title.poster} alt="poster" style={{ width: '100px', height: 'auto' }} />}
               </p>
             </li>
           ))}
@@ -137,7 +194,10 @@ const UserPage = () => {
           {searchHistory.items.map((historyItem) => (
             <li key={historyItem.id}>
               <p>
-                <strong>Search Query:</strong> {historyItem.searchQuery}
+                <strong>Search Query:</strong> {" "}
+                <Link to={`/search?searchTerm=${encodeURIComponent(historyItem.searchQuery)}`}>
+                {historyItem.searchQuery}
+              </Link>
               </p>
               <p>
                 <strong>ID:</strong> {historyItem.id}
