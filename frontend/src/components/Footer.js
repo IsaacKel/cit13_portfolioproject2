99d2131097bbf8e9312643a9c81ea02981f9121e
IsaacKel <s3931572@student.rmit.edu.au>
2024-12-10 10:37:17 +0100

    bookmark conditional rendering

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-center py-3">
-      <p style={{color: 'white'}}>Copyright &copy; 2024. All Rights Reserved.</p>
+    <footer className="bg-dark text-center py-3" style={{ borderTop: "none" }}>
+      <p style={{ color: "white" }}>
+        Copyright &copy; 2024. All Rights Reserved.
+      </p>
     </footer>
   );
 };
