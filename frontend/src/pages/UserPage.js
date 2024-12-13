b33da7dec9a64213e76781d6221a0ae1d259dfae
IsaacKel <s3931572@student.rmit.edu.au>
2024-12-13 14:56:49 +0100

    progress on user page

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -13,6 +13,7 @@ import {
 } from "../services/apiService";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../components/AuthContext";
+import "./UserPage.css";
 
 const UserPage = () => {
   const navigate = useNavigate();
@@ -23,13 +24,13 @@ const UserPage = () => {
   const [searchHistory, setSearchHistory] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedSection, setSelectedSection] = useState("user-details");
 
-   useEffect(() => {
+  useEffect(() => {
     if (!loadingAuth && !isLoggedIn) {
       navigate("/login"); // Redirect to the login page if not logged in
     }
   }, [isLoggedIn, navigate]);
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,9 +42,9 @@ const UserPage = () => {
             fetchUserRatings(),
             fetchUserSearchHistory(),
           ]);
-  
+
         setUser(userData || {});
-  
+
         const bookmarksWithTitles = await Promise.all(
           userBookmarks.items.map(async (bookmark) => {
             if (bookmark.tConst) {
@@ -61,7 +62,7 @@ const UserPage = () => {
       } catch (err) {
         console.error(err);
       }
-  
+
       try {
         const userRatings = await fetchUserRatings();
         const ratingsWithTitles = await Promise.all(
@@ -70,23 +71,21 @@ const UserPage = () => {
             return { ...rating, title: titleData };
           })
         );
-        //console.log(ratingsWithTitles);
         setRatings(ratingsWithTitles || []);
       } catch (err) {
         setRatings([]);
       }
-  
+
       try {
         const userSearchHistory = await fetchUserSearchHistory();
         setSearchHistory(userSearchHistory || []);
-        //console.log(userSearchHistory);
       } catch (err) {
         setSearchHistory([]);
       }
-  
+
       setLoading(false);
     };
-  
+
     fetchData();
   }, []);
 
@@ -98,20 +97,24 @@ const UserPage = () => {
       setError(err.message);
     }
   };
+
   const handleDeleteBookmark = async (bookmarkId) => {
     try {
       await deleteBookmark(bookmarkId);
       setBookmarks((prevBookmarks) => ({
-        items: prevBookmarks.items.filter((b) => b.id !== bookmarkId)
+        items: prevBookmarks.items.filter((b) => b.id !== bookmarkId),
       }));
     } catch (err) {
       setError(err.message);
     }
   };
+
   const handleDeleteSearchHistory = async (searchHistoryId) => {
     try {
       await deleteSearchHistory(searchHistoryId);
-      setSearchHistory((prevSearchHistory) => ({ items: prevSearchHistory.items.filter((s) => s.id !== searchHistoryId) }));
+      setSearchHistory((prevSearchHistory) => ({
+        items: prevSearchHistory.items.filter((s) => s.id !== searchHistoryId),
+      }));
     } catch (err) {
       setError(err.message);
     }
@@ -121,144 +124,197 @@ const UserPage = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div style={{color: 'white'}}>
-      <h1>User Page</h1>
-      {user && (
-        <div>
-          <p>
-            <strong>ID:</strong> {user.id}
-          </p>
-          <p>
-            <strong>Email:</strong> {user.email}
-          </p>
-          <p>
-            <strong>Username:</strong> {user.username}
-          </p>
-          {/* Add more user fields as needed */}
-        </div>
-      )}
-
-      <h2>Bookmarks</h2>
-      {bookmarks.items && bookmarks.items.length > 0 ? (
-        <ul>
-          {bookmarks.items.map((bookmark) => (
-            <li key={bookmark.id}>
-              {bookmark.tConst ? (
-                <>
-                  <strong>Title:</strong>{" "}
-                  <Link to={`/title/${bookmark.tConst}`}>
-                    {bookmark.tConst}
-                  </Link>
-                  <p>
-                    <strong>Title Name:</strong> {bookmark.title.primaryTitle}
-                  </p>
-                  <p>
-                    <strong>Title Type:</strong> {bookmark.title.titleType}
-                  </p>
-                  <p>
-                    <strong>Poster: </strong>{<img src={bookmark.title.poster} alt="poster" style={{ width: '100px', height: 'auto' }} />}
-                  </p>
-                </>
-              ) : (
-                <>
-                  <strong>Name:</strong>{" "}
-                  <Link to={`/name/${bookmark.nConst}`}>{bookmark.nConst}</Link>
-                  <p>
-                    <strong>Name:</strong> {bookmark.name.actualName}
-                  </p>
-                  <p>
-                    <strong>Birth Year:</strong>{bookmark.name.birthYear}
-                  </p>
-                  <p>
-                    <strong>Death Year:</strong>{bookmark.name.deathYear}
-                  </p>
-                  <p>
-                    <string>Rating: </string>{bookmark.name.nRating}
-                  </p>
-                  <p>
-                    <strong>Image: </strong>{<img src={bookmark.image} alt="image" style={{ width: '100px', height: 'auto' }} />}
-                  </p>
-                </>
-              )}
-              <p>
-                <strong>Note:</strong> {bookmark.note}
-              </p>
-              <p>
-                <strong>Date:</strong>{" "}
-                {new Date(bookmark.createdAt).toLocaleDateString("en-GB")}
-              </p>
-              <p>
-                <strong>ID:</strong> {bookmark.id}
-              </p>
-              <button onClick={() => handleDeleteBookmark(bookmark.id)}> Delete Bookmark</button>
-            </li>
-          ))}
+    <div className="container-fluid">
+      <nav className="sidebar">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedSection === "user-details" ? "active" : ""
+              }`}
+              href="#user-details"
+              onClick={() => setSelectedSection("user-details")}
+            >
+              User Details
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedSection === "bookmarks" ? "active" : ""
+              }`}
+              href="#bookmarks"
+              onClick={() => setSelectedSection("bookmarks")}
+            >
+              Bookmarks
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedSection === "ratings" ? "active" : ""
+              }`}
+              href="#ratings"
+              onClick={() => setSelectedSection("ratings")}
+            >
+              Ratings
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedSection === "search-history" ? "active" : ""
+              }`}
+              href="#search-history"
+              onClick={() => setSelectedSection("search-history")}
+            >
+              Search History
+            </a>
+          </li>
         </ul>
-      ) : (
-        <p>No bookmarks found.</p>
-      )}
+      </nav>
+      <div className="main-content">
+        {selectedSection === "user-details" && (
+          <div>
+            <h2>Your Details</h2>
+            {user && (
+              <>
+                <p>
+                  <strong>Email:</strong> {user.email}
+                </p>
+                <p>
+                  <strong>Username:</strong> {user.username}
+                </p>
+              </>
+            )}
+          </div>
+        )}
+        {selectedSection === "bookmarks" && (
+          <>
+            <h2>Bookmarks</h2>
+            {bookmarks.items && bookmarks.items.length > 0 ? (
+              <div className="user-card-grid">
+                {bookmarks.items.map((bookmark) => (
+                  <div className="user-card" key={bookmark.id}>
+                    {bookmark.tConst ? (
+                      <>
+                        <p>
+                          <Link to={`/title/${bookmark.tConst}`}>
+                            {bookmark.title.primaryTitle}
+                          </Link>
+                        </p>
+                        <img src={bookmark.title.poster} alt="poster" />
+                      </>
+                    ) : (
+                      <>
+                        <p>
+                          <Link to={`/name/${bookmark.nConst}`}>
+                            {bookmark.name.actualName}
+                          </Link>
+                        </p>
+                        <img src={bookmark.image} alt="person" />
+                      </>
+                    )}
+                    <p>
+                      <strong>Note:</strong> {bookmark.note}
+                    </p>
+                    <p>
+                      <strong>Date:</strong>{" "}
+                      {new Date(bookmark.createdAt).toLocaleDateString("en-GB")}
+                    </p>
+                    <button onClick={() => handleDeleteBookmark(bookmark.id)}>
+                      Delete Bookmark
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No bookmarks found.</p>
+            )}
+          </>
+        )}
 
-      <h2>Ratings</h2>
-      {ratings && ratings.length > 0 ? (
-        <ul>
-          {ratings.map((rating) => (
-            <li key={rating.id}>
-              <p>
-                <strong>Title ID:</strong>{" "}
-                <Link to={`/title/${rating.tConst}`}>{rating.tConst}</Link>
-              </p>
-              <p>
-                <strong>Rating:</strong> {rating.rating}
-              </p>
-              <p>
-                <strong>ID:</strong> {rating.id}
-              </p>
-              <p>
-                <strong>Date:</strong>{" "}
-                {new Date(rating.createdAt).toLocaleDateString("en-GB")}
-              </p>
-              <p>
-                <strong>Title Name:</strong> {rating.title.primaryTitle}
-              </p>
-              <p>
-                <strong>Title Type:</strong> {rating.title.titleType}
-              </p>
-              <p>
-                <strong>Poster: </strong>{<img src={rating.title.poster} alt="poster" style={{ width: '100px', height: 'auto' }} />}
-              </p>
-              <button onClick={() => handleDeleteRating(rating.userId, rating.id)}> Delete Rating</button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No ratings found.</p>
-      )}
-
-      <h2>Search History</h2>
-      {searchHistory.items && searchHistory.items.length > 0 ? (
-        <ul>
-          {searchHistory.items.map((historyItem) => (
-            <li key={historyItem.id}>
-              <p>
-                <strong>Search Query:</strong> {" "}
-                <Link to={`/search?searchTerm=${encodeURIComponent(historyItem.searchQuery)}`}>
-                {historyItem.searchQuery}
-              </Link>
-              </p>
-              <p>
-                <strong>ID:</strong> {historyItem.id}
-              </p>
-              <p>
-                <strong>Date:</strong>{" "}
-                {new Date(historyItem.createdAt).toLocaleDateString("en-GB")}
-              </p>
-              <button onClick={() => handleDeleteSearchHistory(historyItem.id)}> Delete Search History</button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No search history found.</p>
-      )}
+        {selectedSection === "ratings" && (
+          <>
+            <h2>Ratings</h2>
+            {ratings && ratings.length > 0 ? (
+              <ul>
+                {ratings.map((rating) => (
+                  <li key={rating.id}>
+                    <p>
+                      <Link to={`/title/${rating.tConst}`}>
+                        {rating.title.primaryTitle}
+                      </Link>
+                    </p>
+                    <p>
+                      <strong>Rating:</strong> {rating.rating}
+                    </p>
+                    <p>
+                      <strong>Date:</strong>{" "}
+                      {new Date(rating.createdAt).toLocaleDateString("en-GB")}
+                    </p>
+                    <p>
+                      <strong>Title Type:</strong> {rating.title.titleType}
+                    </p>
+                    <p>
+                      <img
+                        src={rating.title.poster}
+                        alt="poster"
+                        style={{ width: "100px", height: "auto" }}
+                      />
+                    </p>
+                    <button
+                      onClick={() =>
+                        handleDeleteRating(rating.userId, rating.id)
+                      }
+                    >
+                      Delete Rating
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No ratings found.</p>
+            )}
+          </>
+        )}
+        {selectedSection === "search-history" && (
+          <>
+            <h2>Search History</h2>
+            {searchHistory.items && searchHistory.items.length > 0 ? (
+              <ul>
+                {searchHistory.items.map((historyItem) => (
+                  <li key={historyItem.id}>
+                    <p>
+                      <strong>Query:</strong>{" "}
+                      <Link
+                        to={`/search?searchTerm=${encodeURIComponent(
+                          historyItem.searchQuery
+                        )}`}
+                      >
+                        {historyItem.searchQuery}
+                      </Link>
+                    </p>
+                    <p>
+                      <strong>Date:</strong>{" "}
+                      {new Date(historyItem.createdAt).toLocaleDateString(
+                        "en-GB"
+                      )}
+                    </p>
+                    <button
+                      onClick={() => handleDeleteSearchHistory(historyItem.id)}
+                    >
+                      Delete Search History
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No search history found.</p>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
